22c7bae411f87546edc3bee0106062748f4fd651
Christian Graber <cg94301@gmx.com>
2018-06-07 15:29:20 -0700

    add commented out settings for deployment

--- instance/settings.py
+++ instance/settings.py
@@ -1,5 +1,9 @@
+DEBUG = True
+
 MAIL_USERNAME = 'cgraber@gmail.com'
 MAIL_PASSWORD = '8978N+Aas'
 
 STRIPE_SECRET_KEY = 'sk_test_d6sQ0pKlkxkfLIGgZWw37rfm'
 STRIPE_PUBLISHABLE_KEY = 'pk_test_K3zI38kZaXh2l0MqkMMh9VXP'
+
+#SERVER_NAME = 'ec2-54-244-24-70.us-west-2.compute.amazonaws.com'
