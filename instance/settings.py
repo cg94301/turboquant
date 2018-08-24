8c2948f9f2b9e98d79803984d5cfc314a95ef8f5
Christian Graber <cg94301@gmx.com>
2018-08-23 22:14:13 -0700

    alternative urls

--- instance/settings.py
+++ instance/settings.py
@@ -7,3 +7,5 @@ STRIPE_SECRET_KEY = 'sk_test_d6sQ0pKlkxkfLIGgZWw37rfm'
 STRIPE_PUBLISHABLE_KEY = 'pk_test_K3zI38kZaXh2l0MqkMMh9VXP'
 
 #SERVER_NAME = 'ec2-34-244-24-70.us-west-2.compute.amazonaws.com:8000'
+#SERVER_NAME = 'turboquant.com:8000'
+#SERVER_NAME = '2e11e97c.ngrok.io'
